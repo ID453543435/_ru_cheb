3f8977804ac231e2f8659210f187940a6f7b1645
ID453543435 <g5sfg32@yopmail.com>
2014-08-02 17:22:01 +0700

    [version=1.26|28]

--- web_mashine/post_form.pl
+++ web_mashine/post_form.pl
@@ -33,32 +33,35 @@ sub main {
         -method  => 'POST',
 #        -enctype => &CGI::URL_ENCODED,
 #        -onsubmit => 'return javascript:validation_function()',
-        -action => 'data_srnd.pl', # Defaults to 
+        -action => 'data_send.pl', # Defaults to 
                                                  # the current program
-    );    
+    ),"\n";    
 
+    print "point_id:";
     print $m_cgi::cgi->textfield(
         -name      => 'point_id',
         -value     => $point_id,
         -size      => 20,
         -maxlength => 30,
-    );
+    ),"\n";
 
+    print "data:";
     print $m_cgi::cgi->filefield(
         -name => 'data',
-    );
+    ),"\n";
 
+    print "data_arx:";
     print $m_cgi::cgi->filefield(
         -name => 'data_arx',
-    );    
+    ),"\n";    
     
     print $m_cgi::cgi->submit(
         -name     => 'submit_form',
         -value    => 'POST',
 #        -onsubmit => 'javascript: validate_form()',
-    );
+    ),"\n";
 
-    print $m_cgi::cgi->end_form;
+    print $m_cgi::cgi->end_form,"\n";
 
 
     $m_cgi::db->disconnect();
