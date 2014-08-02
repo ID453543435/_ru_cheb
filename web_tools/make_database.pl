d4429695e6e9b82e390957bfc812fbcc1660bc70
ID453543435 <g5sfg32@yopmail.com>
2014-08-02 16:02:35 +0700

    [version=1.26|17]

---
+++ web_tools/make_database.pl
@@ -0,0 +1,64 @@
+#!"D:\xampp\perl\bin\perl.exe"
+#------------------------------------------------------
+=head1 NAME
+base
+=head1 SYNOPSIS
+
+=cut
+#------------------------------------------------------
+     use DBI;
+     use strict;
+#------------------------------------------------------
+# null
+#------------------------------------------------------
+sub null {
+    my ($par)=@_;
+
+
+    return $par;
+}
+#------------------------------------------------------
+# main
+#------------------------------------------------------
+sub main {
+
+     # MySQL database configurations
+     my $dsn = "DBI:mysql:perlmysqldb";
+     my $username = "root";
+     my $password = '';
+
+     # connect to MySQL database
+     my %attr = (PrintError=>0, RaiseError=>1);
+
+     my $dbh = DBI->connect($dsn,$username,$password, \%attr);
+
+     $dbh->do("
+     CREATE TABLE points (
+     id   int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     info varchar(255) NOT NULL
+     );
+     ");
+
+#        (run_number, car_number, date_time, data) 
+
+     $dbh->do("
+     CREATE TABLE data (
+     point_id   int(8) ,
+     run_number int(8) ,
+     car_number int(8) ,
+     date_time  DATETIME ,
+     data       varchar(65) ,
+     KEY k001 (date_time),
+     KEY k002 (point_id,date_time),
+     KEY k003 (point_id,run_number,car_number)
+     );
+     ");
+
+
+     # disconnect from the MySQL database
+     $dbh->disconnect();
+
+}
+#------------------------------------------------------
+$|++;
+main();
