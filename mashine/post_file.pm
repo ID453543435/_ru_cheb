10d2fb395ea85005d13a4c3f1a0c269b75c07974
ID453543435 <g5sfg32@yopmail.com>
2014-08-27 01:01:27 +0700

    [version=1.37|5]

--- mashine/post_file.pm
+++ mashine/post_file.pm
@@ -129,6 +129,7 @@ DATA
         {
             while(1)
             {
+                print "($dateHour le ${file_db::lastDateHour})\n";
                 die unless ($dateHour le $file_db::lastDateHour);
 
                 $dateHour=nextHour($dateHour);
