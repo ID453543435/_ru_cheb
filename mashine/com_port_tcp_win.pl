42e7fa29b034ba261888083573084b1dcbb24419
ID453543435 <g5sfg32@yopmail.com>
2014-10-08 13:55:16 +0700

    [version=1.37|74]

--- mashine/com_port_tcp_win.pl
+++ mashine/com_port_tcp_win.pl
@@ -71,16 +71,16 @@
         my $string_in="";
         my $count_in = 0;
 
-        print "<";
+#        print "<";
 
-        my @ready = $select->can_read( 6 );
+        my @ready = $select->can_read( 1 );
         if (@ready > 0) {
             $count_in = $socket->sysread($string_in,$bytes);
         } else {
 #            warn "data not available on socket now";
         }        
 
-        print "$count_in>";
+#        print "$count_in>";
 
         return ($count_in, $string_in);
     }
