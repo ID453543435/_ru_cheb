7360fc208e1a7166d86811f4f92d5ba1616bf761
ID453543435 <g5sfg32@yopmail.com>
2014-10-17 12:02:44 +0700

    [version=1.37|108]

--- mashine/settings/settings.pl
+++ mashine/settings/settings.pl
@@ -1,8 +1,8 @@
-$server_url="http://37.232.170.76/cgi-bin/web_mashine/";
+$server_url="http://94.232.56.19:8080/cgi-bin/web_mashine/";
 
 $com_port="tcp_win";  #"tcp", "win"
-$com_port_adres="192.168.1.60";
+$com_port_adres="192.168.1.1";
 $com_port_port=503;
 
 
-@dev_adress=(0x10,0x0f);
+@dev_adress=(0x10,0x0f,0x0e);
