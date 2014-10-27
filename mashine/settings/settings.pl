2f8bf754d6c8ad6ef3e47dfa3ef5c2b06a1028b1
ID453543435 <g5sfg32@yopmail.com>
2014-10-27 17:50:25 +0700

    [version=1.37|113]

--- mashine/settings/settings.pl
+++ mashine/settings/settings.pl
@@ -1,10 +1,10 @@
 $server_url="http://94.232.56.19:8080/cgi-bin/web_mashine/";
 
-$com_port="radar";  #"tcp", "win", "tcp_win", "radar"
+$com_port="tcp_win";  #"tcp", "win", "tcp_win", "radar"
 $com_port_adres="192.168.1.1";
 $com_port_port=503;
 
-$radar_adres="localhost";
+$radar_adres="213.87.81.32";
 $radar_port=2001;
 
 
