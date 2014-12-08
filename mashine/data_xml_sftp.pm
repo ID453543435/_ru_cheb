e89fd06b33b0bc0cd234bea2ee88655c4cdd5e5a
ID453543435 <g5sfg32@yopmail.com>
2014-12-09 05:01:27 +0700

    xml[version=1.39|20]

--- mashine/data_xml_sftp.pm
+++ mashine/data_xml_sftp.pm
@@ -59,7 +59,22 @@
                my %sensors=%$j;
 
 
+# =1000(V*t)/N (в метрах)
+# V- Средняя скорость потока за период времени t
+# t-период времени (в часах)
+# N- количество машин за период времени t               
+               my $periodHours=$parameters::data_xml_sftp_send_period/(60*60);
+               $sensors{traffic_average_speed}=$sensors{speed}/$sensors{traffic_total_amount};
+               $sensors{traffic_average_gap}=
+               1000*($sensors{traffic_average_speed}*$periodHours)
+               /$sensors{traffic_total_amount};
+               $sensors{traffic_occupancy}=($sensors{traffic_total_amount}/$periodHours)/$parameters::data_xml_sftp_carpass_max[$chanel];
+               $sensors{measure_period}=$parameters::data_xml_sftp_send_period/60;
+
+
                while (my ($sensor, $value) = each(%sensors)){
+
+#                   next unless substr($sensor,0,8) eq "traffic_";
 
                    my %values=
                    (
@@ -117,8 +132,8 @@
 #        my $chanDir=$data{$chenel}->{$directtion};
 
         $data{$chenel}->{$directtion}->{speed} += $speed;
-        $data{$chenel}->{$directtion}->{total_amount}++;
-        $data{$chenel}->{$directtion}->{$class}++;
+        $data{$chenel}->{$directtion}->{traffic_total_amount}++;
+        $data{$chenel}->{$directtion}->{"traffic_$class"}++;
 
 #        (run_number, car_number, date_time, data) 
 #        ($parameters::run_number,$dbCarNumber,$timeL,$data)
