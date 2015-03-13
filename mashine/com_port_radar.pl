3f0771bf2b35feef60d5e9ffa3d6ab0fbd6a36cd
ID453543435 <g5sfg32@yopmail.com>
2015-03-13 17:42:36 +0700

    radar[version=1.39|49]

--- mashine/com_port_radar.pl
+++ mashine/com_port_radar.pl
@@ -12,78 +12,10 @@
 #------------------------------------------------------
 #    package com_port;
 
-    use vars qw($socket $select);
-#------------------------------------------------------
-# null
-#------------------------------------------------------
-    sub null {
-        my ($par)=@_;
+    $parameters::com_port_adres=$parameters::radar_adres;
+    $parameters::com_port_port=$parameters::radar_port;
 
+    do("com_port_tcp_win_reconnect.pl");
 
-        return $par;
-    }
-#------------------------------------------------------
-# openPort
-#------------------------------------------------------
-    sub openPort {
-        
-        $socket = new IO::Socket::INET (
-             PeerAddr => $parameters::radar_adres,
-             PeerPort => $parameters::radar_port,
-             Proto => 'tcp',
-        ) or die "ERROR in Socket Creation : $!\n";
-
-        binmode($socket);
-
-        $select = IO::Select->new;
-        $select->add( $socket );
-
-        return;
-    }
-#------------------------------------------------------
-# closePort
-#------------------------------------------------------
-    sub closePort {
-
-        $socket->close || die "failed to close";
-
-        return;
-    }
-#------------------------------------------------------
-# sendData
-#------------------------------------------------------
-    sub sendData {
-        my ($data)=@_;
-
-        my $count_out = $socket->syswrite($data);
-#        warn "write failed\n"         unless ($count_out);
-#        warn "write incomplete\n"     if ( $count_out != length($data) );  
-
-#        sleep(0.1);
-
-        return $count_out;
-    }
-#------------------------------------------------------
-# readData
-#------------------------------------------------------
-    sub readData {
-        my ($bytes)=@_;
-
-        my $string_in="";
-        my $count_in = 0;
-
-#        print "<";
-
-        my @ready = $select->can_read( 1 );
-        if (@ready > 0) {
-            $count_in = $socket->sysread($string_in,$bytes);
-        } else {
-#            warn "data not available on socket now";
-        }        
-
-#        print "$count_in>";
-
-        return ($count_in, $string_in);
-    }
 #------------------------------------------------------
 #1;
