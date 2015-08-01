4f0f260fadb85eada06544d6a4ba949caa61fd50
ID453543435 <g5sfg32@yopmail.com>
2015-08-01 19:23:52 +0700

    [version=1.39|71]

--- data_xml_sftp/settings.pm
+++ data_xml_sftp/settings.pm
@@ -11,6 +11,8 @@
     package settings;
 #------------------------------------------------------
 
+=head1 SYNOPSIS
+
     use vars qw($point_code $run_number $server_url);
     use vars qw($com_port $com_port_adres $com_port_port);
     use vars qw($com_port_linux_file $com_port_speed);
@@ -19,7 +21,24 @@
     use vars qw($data_xml_sftp_enable);
     use vars qw($data_xml_sftp_adress $data_xml_sftp_port $data_xml_sftp_user $data_xml_sftp_password);
     use vars qw($data_xml_sftp_send_point_id $data_xml_sftp_send_period @data_xml_sftp_carpass_max);
+=cut
     
+    use vars qw(@_vars);
+
+    @_vars=();
+    push @_vars, qw($point_code $run_number $server_url);
+    push @_vars, qw($com_port $com_port_adres $com_port_port);
+    push @_vars, qw($com_port_linux_file $com_port_speed);
+    push @_vars, qw($pribor @dev_adress);
+
+    push @_vars, qw($data_xml_sftp_enable);
+    push @_vars, qw($data_xml_sftp_adress $data_xml_sftp_port $data_xml_sftp_user $data_xml_sftp_password);
+    push @_vars, qw($data_xml_sftp_send_point_id $data_xml_sftp_send_period @data_xml_sftp_carpass_max);
+
+    use vars @_vars;
+
+    use vars qw(%_data);
+
 #------------------------------------------------------
 # null
 #------------------------------------------------------
@@ -37,6 +56,22 @@
 
         do "lib/settings_base.pl";
         do $file;
+
+        return;
+    }
+#------------------------------------------------------
+# saveCurrent
+#------------------------------------------------------
+    sub saveCurrent {
+        my ($pointId)=@_;
+
+        $_data{$pointId}={};
+        for my $i (@_vars)
+        {
+           $$_data{$pointId}{$i}=
+
+        }
+
 
         return;
     }
